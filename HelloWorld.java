54c16c37bfd4ae619d679889264eedf522ce0918
Praveen Kotaru <praveenkotaru@gmail.com>
2024-12-23 18:21:10 -0500

    Update the java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,15 @@
 public class HelloWorld {
+    static int a = 10;
+    static int b = 11;
+
     public static void main(String[] args) {
         System.out.println("Hello world");
+        HelloWorld.add();
     }
 
-}
+    public static void add() {
+        int c = a + b;
+        System.out.println("Sum of two numbers " + c);
 
+    }
+}
