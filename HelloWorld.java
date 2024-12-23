352158d29d19f0dd56fb7fc35629311c430b2d69
Praveen Kotaru <praveenkotaru@gmail.com>
2024-12-23 18:52:33 -0500

    Updated the code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
+
 public class HelloWorld {
-    static int a = 10;
-    static int b = 11;
+    static int a;
+    static int b;
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        HelloWorld.add();
+        HelloWorld hello = new HelloWorld();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number");
+        a = sc.nextInt();
+        System.out.println("Enter second number");
+        b = sc.nextInt();
+        hello.add();
     }
 
-    public static void add() {
+    public void add() {
         int c = a + b;
         System.out.println("Sum of two numbers " + c);
 
